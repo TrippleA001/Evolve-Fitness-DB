ad410588e490e5431c4265a5428c2de1fd7c06d2
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-05-29 12:19:10 -0700

    email extractions

---
+++ 0.2 Deeper Dives & Business Analytics/Q14.sql
@@ -0,0 +1,8 @@
+USE evolve_fitness;
+
+-- Q14: Generate a combined list of contact emails. Include emails from all 'Active' members and all instructors. 
+-- Call the resulting column ContactEmail. Then ensure each email appears only once, even if an instructor is also 
+-- an active member.
+SELECT Email AS ContactEmail FROM Members WHERE MembershipStatus = 'Active'
+UNION
+SELECT Email AS ContactEmail FROM Instructors;
