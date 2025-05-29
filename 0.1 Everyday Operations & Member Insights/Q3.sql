6239b2221941c4f664f991a338e9f904d2098fad
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-05-29 10:42:44 -0700

    members whose payments are due

---
+++ 0.1 Everyday Operations & Member Insights/Q3.sql
@@ -0,0 +1,5 @@
+-- Q3: Show the FirstName, LastName, and NextPaymentDueDate for all members whose next payment is due on or before 
+-- June 10, 2025.
+SELECT FirstName, LastName, NextPaymentDueDate
+FROM Members
+WHERE NextPaymentDueDate <= '2025-06-10';
