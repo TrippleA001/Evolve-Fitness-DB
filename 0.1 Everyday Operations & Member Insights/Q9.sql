19b9a019dc5395a146de0269532cb059fb837e99
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-05-29 10:57:04 -0700

    classes Instructor Jane Doe take

---
+++ 0.1 Everyday Operations & Member Insights/Q9.sql
@@ -0,0 +1,5 @@
+-- Q9: How many classes is Instructor 'Jane Doe' scheduled to teach?
+SELECT COUNT(*) AS ClassesTaught
+FROM classes c
+JOIN Instructors i ON c.InstructorID = i.InstructorID
+WHERE i.FirstName = 'Jane' AND i.LastName = 'Doe';
