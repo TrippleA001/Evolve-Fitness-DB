e53fa40d3be528cadba9f8ead7f6994c8607d7f3
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-05-29 11:27:11 -0700

    instructors list

---
+++ 0.1 Everyday Operations & Member Insights/Q10.sql
@@ -0,0 +1,5 @@
+-- Q10: List all instructors. If an instructor's phone number (Phone) is missing (NULL) in the Instructors table, 
+-- display 'Not Provided' instead. Show the instructor's FirstName, LastName, Email, and the modified Phone.
+SELECT FirstName, LastName, Email, 
+       COALESCE(Phone, 'Not Provided') AS Phone
+FROM Instructors;
