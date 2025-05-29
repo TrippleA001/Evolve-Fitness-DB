b02048c6f9f926176775d4447c826a948d286724
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-05-29 11:21:07 -0700

    members who registered after January

---
+++ 0.1 Everyday Operations & Member Insights/Q4.sql
@@ -0,0 +1,5 @@
+-- Q4: Find all members who registered on or after January 1, 2025. Display their FirstName, LastName, 
+-- and RegistrationDate.
+SELECT FirstName, LastName, RegistrationDate
+FROM Members
+WHERE RegistrationDate >= '2025-01-01';
