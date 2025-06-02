6a2067baa3099d9415e46d54b7994351224e714a
ademipeju <ademipeju5@gmail.com>
2025-06-03 00:20:30 +0100

    question 16

---
+++ 0.2 Deeper Dives & Business Analytics/Q16.sql
@@ -0,0 +1,9 @@
+-- 16. Which MembershipTier has generated the most revenue from 'Paid' payments so far?
+-- Show the TierName and total revenue
+SELECT mt.TierName, SUM(m.TotalPaymentsMade) AS TotalRevenue
+FROM members m
+JOIN membership_tiers mt ON m.TierID = mt.TierID
+WHERE m.membership_status = 'Active'
+GROUP BY mt.TierName
+ORDER BY TotalRevenue DESC
+LIMIT 1;
