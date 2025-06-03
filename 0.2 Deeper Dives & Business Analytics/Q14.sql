5350460bb0786d6fc11d9e407f624e9946c89f24
Saudat Abubakar <abubakarsaudat52@gmail.com>
2025-06-03 12:57:14 +0100

    Update Q14.sql

--- 0.2 Deeper Dives & Business Analytics/Q14.sql
+++ 0.2 Deeper Dives & Business Analytics/Q14.sql
@@ -2,4 +2,3 @@
 -- Q14: SELECT Email AS ContactEmail FROM Members WHERE MembershipStatus = 'Active'
 UNION
 SELECT Email AS ContactEmail FROM Instructors;
->>>>>>> ad410588e490e5431c4265a5428c2de1fd7c06d2
