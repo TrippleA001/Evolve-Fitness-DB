0312ac869c2e2dfe424a4b201190b37ed4ae9b65
realaisha <realaisha1@gmail.com>
2025-06-04 14:10:44 +0100

    adding question 21

--- 0.2 Deeper Dives & Business Analytics/Q22.sql
+++ 0.2 Deeper Dives & Business Analytics/Q22.sql
@@ -1,11 +1,11 @@
 SELECT 
   c.ClassName,
-  sc.ClassDate,
-  sc.ClassTime,
+  cs.ClassDate,
+  cs.ClassTime,
   CASE
-    WHEN sc.MaxCapacity < 15 THEN 'Small'
-    WHEN sc.MaxCapacity BETWEEN 15 AND 25 THEN 'Medium'
-    WHEN sc.MaxCapacity > 25 THEN 'Large'
+    WHEN c.MaxCapacity < 15 THEN 'Small'
+    WHEN c.MaxCapacity BETWEEN 15 AND 25 THEN 'Medium'
+    WHEN c.MaxCapacity > 25 THEN 'Large'
   END AS CapacityCategory
-FROM scheduled_classes sc
-JOIN classes c ON sc.ClassID = c.ClassID;
+FROM class_sessions cs
+JOIN classes c ON cs.ClassID = c.ClassID;
