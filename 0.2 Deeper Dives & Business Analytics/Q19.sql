60739e1a06263dd6e5d345ec5fecc2b5bff78841
realaisha <realaisha1@gmail.com>
2025-06-04 13:47:02 +0100

    adding question 19

--- 0.2 Deeper Dives & Business Analytics/Q19.sql
+++ 0.2 Deeper Dives & Business Analytics/Q19.sql
@@ -2,7 +2,7 @@ SELECT
   c.ClassName,
   COUNT(b.BookingID) AS TotalBookings
 FROM bookings b
-JOIN scheduled_classes sc ON b.ScheduledClassID = sc.ScheduledClassID
-JOIN classes c ON sc.ClassID = c.ClassID
+JOIN class_sessions cs ON b.SessionID = cs.SessionID
+JOIN classes c ON cs.ClassID = c.ClassID
 GROUP BY c.ClassName
 ORDER BY TotalBookings DESC;
