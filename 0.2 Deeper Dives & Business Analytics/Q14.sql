bdbf174a524d0a4af61cefbf31c8c0a2655bc2be
Hajarat OLUFADE <olufadehajarattitilope@gmail.com>
2025-06-03 18:12:41 -0700

    modified

--- 0.2 Deeper Dives & Business Analytics/Q14.sql
+++ 0.2 Deeper Dives & Business Analytics/Q14.sql
@@ -1,5 +1,3 @@
-USE evolve_fitness;
-
 -- Q14: Generate a combined list of contact emails. Include emails from all 'Active' members and all instructors. 
 -- Call the resulting column ContactEmail. Then ensure each email appears only once, even if an instructor is also 
 -- an active member.
